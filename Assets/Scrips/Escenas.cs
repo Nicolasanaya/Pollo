48cf29790f7bc1d5f9588db45d6d5bbb469f9e27
Nicolasanaya <46981964+Nicolasanaya@users.noreply.github.com>
2020-10-07 12:31:50 -0500

    Primer Commit

---
+++ Assets/Scrips/Escenas.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Escenas : MonoBehaviour
+{
+    
+    public void escena1 (string nombreescena)
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void escena2(string nombreescena)
+    {
+        SceneManager.LoadScene(2);
+    }
+
+}
